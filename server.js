9c22dded969713d6c1c6a06584850fce02383a7e
Travis Mingalone <travismingalone@gmail.com>
2019-03-09 12:51:29 -0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
 const cheerio = require('cheerio');
-const util = require('util');
+const dotenv = require('dotenv');
+dotenv.load();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,11 +17,10 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/search', (req, res) => {
-  console.log(req.body);
   var word = req.body.word;
   const song = req.body.song;
   const artist = req.body.artist;
-  axios.get('https://api.genius.com/search?q='+song + ' ' + artist, {headers: {Authorization: 'Bearer DQxOTVSqe4oh2EYdnOz5bfPe1HlK4Yeod7eZ21pPpzKqBw2IIfb8BLg-ZMeuEhEF'}})
+  axios.get('https://api.genius.com/search?q='+song + ' ' + artist, {headers: {Authorization: 'Bearer ' + process.env.GENIUS_TOKEN}})
         .then((response)=>{
           const url = 'http://www.genius.com' + response.data.response.hits[0].result.api_path;
           axios.get(url)
@@ -30,14 +30,15 @@ app.post('/search', (req, res) => {
               return lyrics
             })
             .then(function(lyrics) {
-              console.log(lyrics.split('\\n'))
               const regex = new RegExp(word, 'i');
-              console.log(lyrics.match(regex));
               if(lyrics.match(regex)===null) {
                 res.send('false')
               } else {
                 res.send('true')
               }
+            })
+            .catch(err => {
+              console.log(err)
             })
         })
         .catch((error)=>{
